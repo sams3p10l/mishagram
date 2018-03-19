7a0978f0dd6d316da577b6c0f738dc9482723dfd
sams3p10l <mix996@gmail.com>
2018-03-19 01:49:30 +0100

    register activity update

--- app/src/main/java/com/example/specter/mishagram/RegisterActivity.java
+++ app/src/main/java/com/example/specter/mishagram/RegisterActivity.java
@@ -3,7 +3,9 @@ package com.example.specter.mishagram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +14,9 @@ import android.widget.Spinner;
 public class RegisterActivity extends AppCompatActivity
 {
 
-	public EditText username, password, email;
+	public EditText username, password, email, firstName, lastName;
 	public Button register;
+	protected boolean usernameCheck, passwordCheck, emailCheck;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -24,8 +27,82 @@ public class RegisterActivity extends AppCompatActivity
 		username = findViewById(R.id.username_register);
 		password = findViewById(R.id.password_register);
 		email = findViewById(R.id.email_register);
+		firstName = findViewById(R.id.first_name);
+		lastName = findViewById(R.id.last_name);
 		register = findViewById(R.id.button_register);
 
+		username.setOnClickListener(new View.OnClickListener()				//ZASTO NE RADI
+		{
+			int i = 0;
+
+			@Override
+			public void onClick(View view)
+			{
+				if (++i == 1)
+					username.setText("");
+			}
+		});
+
+		password.setOnFocusChangeListener(new View.OnFocusChangeListener()
+		{
+			int i = 0;
+
+			@Override
+			public void onFocusChange(View view, boolean b)
+			{
+				if (++i == 1)
+				{
+					password.setText("");
+					password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				}
+
+			}
+		});
+
+		firstName.setOnFocusChangeListener(new View.OnFocusChangeListener()
+		{
+			int i = 0;
+
+			@Override
+			public void onFocusChange(View view, boolean b)
+			{
+				if (++i == 1)
+				{
+					firstName.setText("");
+				}
+			}
+		});
+
+		lastName.setOnFocusChangeListener(new View.OnFocusChangeListener()
+		{
+			int i = 0;
+
+			@Override
+			public void onFocusChange(View view, boolean b)
+			{
+				if (++i == 1)
+				{
+					lastName.setText("");
+				}
+			}
+		});
+
+		email.setOnFocusChangeListener(new View.OnFocusChangeListener()
+		{
+			int i = 0;
+
+			@Override
+			public void onFocusChange(View view, boolean b)
+			{
+				if (++i == 1)
+				{
+					email.setText("");
+				}
+			}
+		});
+
+		//TODO: sinhronizuj listenere
+		//da li moraju overrideovane funkcije da bleje prazne
 
 		username.addTextChangedListener(new TextWatcher()
 		{
@@ -44,10 +121,7 @@ public class RegisterActivity extends AppCompatActivity
 			@Override
 			public void afterTextChanged(Editable editable)
 			{
-				if (username.getText().toString().length() == 0)
-					register.setEnabled(false);
-				else
-					register.setEnabled(true);
+				usernameCheck = username.getText().toString().length() > 0;
 			}
 		});
 
@@ -68,10 +142,7 @@ public class RegisterActivity extends AppCompatActivity
 			@Override
 			public void afterTextChanged(Editable editable)
 			{
-				if (password.getText().toString().length() < 6)
-					register.setEnabled(false);
-				else
-					register.setEnabled(true);
+				passwordCheck = password.getText().toString().length() >= 6;
 			}
 		});
 
@@ -97,23 +168,19 @@ public class RegisterActivity extends AppCompatActivity
 			@Override
 			public void afterTextChanged(Editable editable)
 			{
-				if (email.getText().toString().length() == 0 || !monkeyChecker())
-					register.setEnabled(false);
-				else
-					register.setEnabled(true);
+				emailCheck = email.getText().toString().length() > 0 && monkeyChecker();
 			}
 		});
-		
 
 
-
-
-
-		Spinner spinner = (Spinner) findViewById(R.id.gender_spinner);								//gender list pull
+		Spinner spinner = findViewById(R.id.gender_spinner);								//gender list pull
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
 				R.array.gender_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
+
+		boolean buttonReady = usernameCheck && passwordCheck && emailCheck;
+			register.setEnabled(buttonReady);
 
 	}
 }
